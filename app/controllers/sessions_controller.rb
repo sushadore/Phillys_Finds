65a019bd29501aad6f09a26ff493ca1c8fb1801a
Susha Dore <susha.dore@gmail.com>
2017-07-07 14:17:02 -0700

    pass user model tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,1 +1,12 @@
-
+class SessionsController < ApplicationController
+  def create
+    @user = User.authenticate(params[:email], params[:password])
+    if @user
+      flash[:notice] = "You're signed in."
+      session[:user_id] = @user.id
+      redirect_to products_path
+    else
+      flash[:alert] = "Email or password did not match. Please try again."
+      redirect_to signin_path
+    end
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   attr_accessor :password
   validates_confirmation_of :password
   validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   before_save :encrypt_password
 
   def encrypt_password
