63b2608fb0d96382a2262f8434a1ed36195aa67f
Susha Dore <susha.dore@gmail.com>
2017-07-11 13:01:57 -0700

    pass user sign up spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,13 @@
 FactoryGirl.define do
-  
+  factory :product do
+    name 'Irish Flute'
+    description 'Delighfully fluttery'
+    cost '38.99'
+  end
+
+  factory :review do
+    author 'ThisGuy'
+    content 'Chuck Norris is currently suing NBC, claiming Law and Order are trademarked names for his left and right legs.'
+    rating '4'
+  end
 end
